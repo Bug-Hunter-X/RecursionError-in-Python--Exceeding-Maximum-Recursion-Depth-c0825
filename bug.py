d22c29374a7921d1f3cca822dc3423b21ce9247c
BugHunter-X <askzenai@gmail.com>
2024-12-20 00:28:05 +0100

    Add bug.py file

---
+++ bug.py
@@ -0,0 +1,9 @@
+def function_with_uncommon_error(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return function_with_uncommon_error(n-1) + function_with_uncommon_error(n-2)
+
+print(function_with_uncommon_error(35)) # This will cause RecursionError
